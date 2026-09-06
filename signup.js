13256bb60d1acf6839c8f608878c61d239e533a7
zahra <ahmadizahra2005@gmail.com>
2026-09-06 16:03:19 +0330

    Add signup script

---
+++ signup.js
@@ -0,0 +1,24 @@
+const backBtn = document.querySelector("#backBtn");
+
+const fullName = document.querySelector("#name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const confPassword = document.querySelector("#pssword");
+const signupBtn = document.querySelector("#signupBtn");
+
+backBtn.addEventListener("click", function () {
+  window.location.href = "start.html";
+});
+
+signupBtn.addEventListener("click", function () {
+  if (
+    fullName.value.trim() !== "" &&
+    email.value.trim() !== "" &&
+    password.value.trim() !== "" &&
+    confPassword.value.trim() === password.value
+  ) {
+    window.location.href = "tasks.html";
+  } else {
+    alert("Please enter your information.");
+  }
+});
